3559157a7150d81b1304e82f93c4811731e39221
roman <rdandrievskiy@edu.hse.ru>
2023-07-06 21:28:40 +0200

    add edit

--- common/book_api.go
+++ common/book_api.go
@@ -2,8 +2,10 @@ package common
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 	"sync/atomic"
 )
@@ -28,6 +30,24 @@ func StartBookApi() {
 		newBook("Random title", "Random summary")
 		w.WriteHeader(http.StatusOK)
 	})
+
+	http.HandleFunc("/api/books/edit", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		id, title, summary := r.Form["id"], r.Form["title"], r.Form["summary"]
+		if len(id) == 0 || len(title) == 0 || len(summary) == 0 {
+			json.NewEncoder(w).Encode([]string{"error: id, title and summary must be set"})
+		}
+		n, err := strconv.ParseInt(id[0], 10, 64)
+		if err != nil {
+			json.NewEncoder(w).Encode([]string{"id must be a valid int64"})
+		}
+		if err := edit(n, Book{n, title[0], summary[0]}); err != nil {
+			json.NewEncoder(w).Encode([]string{fmt.Sprintf("error: %s", err.Error())})
+		}
+
+		json.NewEncoder(w).Encode([]string{"ok"})
+	})
+
 	port := ":8080"
 	log.Printf("Running on port %s", port)
 	http.ListenAndServe(port, nil)
@@ -43,4 +63,15 @@ func newBook(title, summary string) {
 		Title:   title,
 		Summary: summary,
 	}
+}
+
+func edit(id int64, book Book) error {
+	booksMu.Lock()
+	defer booksMu.Unlock()
+
+	if _, ok := books[id]; !ok {
+		return fmt.Errorf("Book with id %d does not exist", id)
+	}
+	books[id] = book
+	return nil
 }
